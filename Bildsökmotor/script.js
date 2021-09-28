2ccb865b27c1adbad4da1772fed88607dab61c43
nils <n.landegren@hotmail.com>
2021-09-28 16:16:50 +0200

    more javascript functionality

--- Bildsökmotor/script.js
+++ Bildsökmotor/script.js
@@ -1,18 +1,22 @@
 const apiKey = '23524770-cd261b46e16ac6158fc07abc8';
 
-const pictureBoxes = document.querySelectorAll('#grid div img');
+const pictureBoxes = document.querySelectorAll('#grid figure img');
+const figureCaptions = document.querySelectorAll('#grid figure figcaption')
 const previousPage = document.querySelector('#previous-page')
 const nextPage = document.querySelector('#next-page')
 
 let search;
 let color;
 let pageNumber = 1;
-let json;
 let params;
+let limit;
+let json;
+
 const form = document.querySelector('form');
+
 form.onsubmit = async event => {
     event.preventDefault();
-
+   
     search = form.elements.text.value;
     color = form.elements.colors.value;
 
@@ -20,29 +24,35 @@ form.onsubmit = async event => {
         key: apiKey,
         q: search,
         colors: color,
-        page: 1,
+        page: pageNumber,
         per_page: 10
     });
-
     const response = await fetch('https://pixabay.com/api/?' + params.toString());
-    json = await response.json();
-
+    const json = await response.json();
+    limit = Object.keys(json.hits).length;
+    
     document.getElementById("buttoncontainer").style.display = "block";
-    if (json.hits < 10) {
+    if (limit < 10) {
         nextPage.disabled = true;
     }
 
     previousPage.disabled = true;
-    
-    let pageNumber = 1;
-    for (let index = 0; index < 10; index++) {
-        pictureBoxes[index].src = json.hits[index].previewURL;
+    pageNumber = 1;
+    for (let index = 0; index < figureCaptions.length; index++) {
+        figureCaptions[index].textContent = "";
+    }
+    for (let index = 0; index < limit; index++) {
+        pictureBoxes[index].style.display = "block";        
+        figureCaptions[index].textContent = "Tags: " + json.hits[index].tags;
+        figureCaptions[index].textContent += " User: " + json.hits[index].user;
+        pictureBoxes[index].src = json.hits[index].webformatURL;
     }
 }
 
 nextPage.onclick = async event => {
     pageNumber++;
     previousPage.disabled = false;
+
     params = new URLSearchParams({
         key: apiKey,
         q: search,
@@ -51,14 +61,29 @@ nextPage.onclick = async event => {
         per_page: 10
     });
     const response = await fetch('https://pixabay.com/api/?' + params.toString());
-    json = await response.json();
-    for (let index = 0; index < 10; index++) {
-        pictureBoxes[index].src = json.hits[index].previewURL;
+    const json = await response.json();
+    limit = Object.keys(json.hits).length;
+
+    if (limit < 10) {
+        nextPage.disabled = true;
+    }
+    for (let index = 0; index < figureCaptions.length; index++) {
+        figureCaptions[index].textContent = "";
+    }
+    for (let index = 0; index < pictureBoxes.length; index++) {
+        pictureBoxes[index].style.display = "none";
+    }
+    for (let index = 0; index < limit; index++) {
+        pictureBoxes[index].style.display = "block";        
+        figureCaptions[index].textContent = "Tags: " + json.hits[index].tags;
+        figureCaptions[index].textContent += " User: " + json.hits[index].user;
+        pictureBoxes[index].src = json.hits[index].webformatURL;
     }
 }
 
 previousPage.onclick = async event => {
     pageNumber--;
+
     params = new URLSearchParams({
         key: apiKey,
         q: search,
@@ -67,13 +92,43 @@ previousPage.onclick = async event => {
         per_page: 10
     });
     const response = await fetch('https://pixabay.com/api/?' + params.toString());
-    json = await response.json();
-    for (let index = 0; index < 10; index++) {
-        pictureBoxes[index].src = json.hits[index].previewURL;
+    const json = await response.json();
+    limit = Object.keys(json.hits).length;
+    for (let index = 0; index < figureCaptions.length; index++) {
+        figureCaptions[index].textContent = "";
     }
+    for (let index = 0; index < limit; index++) {
+        pictureBoxes[index].style.display = "block";        
+        figureCaptions[index].textContent = "Tags: " + json.hits[index].tags;
+        figureCaptions[index].textContent += " User: " + json.hits[index].user;
+        pictureBoxes[index].src = json.hits[index].webformatURL;
+    }
+
     if (pageNumber === 1) {
         previousPage.disabled = true;
     }
+    nextPage.disabled = false;
 }
 
+
+// function displayImages(){
+//     for (let index = 0; index < limit; index++) {
+//         pictureBoxes[index].src = "";
+//         pictureBoxes[index].src = json.hits[index].previewURL;
+//     }
+// }
+
+// async function makeApiCall(){
+//     params = new URLSearchParams({
+//         key: apiKey,
+//         q: search,
+//         colors: color,
+//         page: pageNumber,
+//         per_page: 10
+//     });
+//     const response = await fetch('https://pixabay.com/api/?' + params.toString());
+//     const json = await response.json();
+//     limit = Object.keys(json.hits).length;
+//     return json;
+// }
 
